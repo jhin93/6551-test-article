3096b198bffa4cd381ca52d83ddd0d57ed840995
jhin93 <scene1993@gmail.com>
2023-09-08 14:47:56 +0900

    다시 원복

--- util/toastConfig.ts
+++ util/toastConfig.ts
@@ -2,8 +2,8 @@ const toastStyle = {
   borderRadius: "4px",
   background: "#222528",
   color: "#fff",
-  "whiteSpace": "pre-wrap",
-  "wordBreak": "break-word",
+  "white-space": "pre-wrap",
+  "word-break": "break-word",
 };
 
 export default toastStyle;
